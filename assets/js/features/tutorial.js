d6af75d70df4b3c89d5b58ba976526450b13cca1
ConseillerDoriantation <37410169+ConseillerDoriantation@users.noreply.github.com>
2026-04-09 14:19:12 +0200

    Update tutorial.js

--- assets/js/features/tutorial.js
+++ assets/js/features/tutorial.js
@@ -52,4 +52,7 @@ async function saveTutorial() {
   }
 }
 
+// Initialiser le namespace si app.js ne l'a pas encore fait
+window.JDRApp = window.JDRApp || {};
+
 Object.assign(window.JDRApp, { getDefaultTutorial, showTutSection, editTutorial, saveTutorial });
